827bd539a43c8297e7e32fd76b56a13fc757198c
martuwilson <williner.dev@gmail.com>
2025-07-13 21:54:39 -0300

    Agregar entidad Pokemon con propiedades y esquema de Mongoose

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,33 @@
-export class Pokemon {}
+import { SchemaFactory, Prop, Schema } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+
+    @Prop({ required: true, unique: true })
+    name: string;
+
+    @Prop({ required: true, unique: true })
+    no: number;
+    
+    @Prop({ required: true })
+    type: string;
+    
+    @Prop({ required: true })
+    level: number;
+    
+    @Prop({ required: true })
+    hp: number;
+    
+    @Prop({ required: true })
+    attack: number;
+    
+    @Prop({ required: true })
+    defense: number;
+    
+    @Prop({ required: true })
+    speed: number;
+
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
